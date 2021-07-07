02c080b5cbdd0b46f7cf1aa637a62e39d72fdcad
Joao Pedro Vilarinho <joao.pedro@sempreceub.com>
2021-07-07 08:59:25 -0300

    Atualização do projeto

---
+++ src/Avanade.SubTCSE.Projeto.Data/Repositories/Employee/EmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Avanade.SubTCSE.Projeto.Data.Repositories.Base;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository;
+using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Data.Repositories.Employee
+{
+    public class EmployeeRepository : BaseRepository<Domain.Aggregates.Employee.Entities.Employee, string>
+    , IBaseRepository
+    { 
+    }
+}
